4599398ae02f1d66f42984ff5652e43582798138
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-14 19:05:18 +0900

    gem letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resource :session, only: %i[new create destroy]
   get "protected" => "home#protected"
   root "home#index"
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
